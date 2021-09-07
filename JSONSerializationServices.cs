2b3d421eec2be9b5cc3794582b5e37438d5ea1b8
Andrew Rober <54873972+AndrewRober@users.noreply.github.com>
2021-09-07 02:32:11 +0200

    up

---
+++ JSONSerializationServices.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SerializationServices.JSON
+{
+    public static class JSONSerializationServices
+    {
+
+        /// <summary>
+        /// Serializes the data as XML in the object to the designated file path
+        /// </summary>
+        /// <typeparam name="T">Type of Object to serialize</typeparam>
+        /// <param name="dataToSerialize">Object to serialize</param>
+        /// <param name="filePath">FilePath for the XML file</param>
+        public static void SerializeToFile<T>(T dataToSerialize, string filePath) =>
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(dataToSerialize));
+
+        public static void SerializeToStream<T>(T dataToSerialize, StreamWriter sw) => 
+            sw.Write(JsonConvert.SerializeObject(dataToSerialize));
+
+        public static async void SerializeToStreamAsync<T>(T dataToSerialize, StreamWriter sw) => 
+            await sw.WriteAsync(JsonConvert.SerializeObject(dataToSerialize));
+
+        public static void SerializeToStream<T>(T dataToSerialize, MemoryStream ms)
+        {
+            using (var sw = new StreamWriter(ms))
+                sw.Write(JsonConvert.SerializeObject(dataToSerialize));
+        }
+
+        public static async void SerializeToStreamAsync<T>(T dataToSerialize, MemoryStream ms)
+        {
+            using (var sw = new StreamWriter(ms))
+                await sw.WriteAsync(JsonConvert.SerializeObject(dataToSerialize));
+        }
+
+        /// <summary>
+        /// Deserializes the data in the XML file into an object
+        /// </summary>
+        /// <typeparam name="T">Type of object to deserialize</typeparam>
+        /// <param name="filePath">FilePath to XML file</param>
+        /// <returns>Object containing deserialized data</returns>
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                return DeserializeFrom<T>(stream);
+        }
+
+        public static T DeserializeFrom<T>(Stream st)
+        {
+            using (var sr = new StreamReader(st))
+                return DeserializeFrom<T>(sr);
+        }
+
+        public static T DeserializeFrom<T>(StreamReader sr)
+        {
+            using (var jsonTextReader = new JsonTextReader(sr))
+                return DeserializeFrom<T>(jsonTextReader);
+        }
+
+        public static T DeserializeFrom<T>(JsonTextReader jsonTextReader)
+        {
+            try
+            {
+                return new JsonSerializer().Deserialize<T>(jsonTextReader);
+            }
+            catch
+            {
+                return Activator.CreateInstance<T>();
+            }
+        }
+    }
+}
---
+++ XMLSerializationServices.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Formatting = System.Xml.Formatting;
+
+// ReSharper disable once CheckNamespace
+namespace SerializationServices.XML
+{
+    public static class XMLSerializationServices
+    {
+        /// <summary>
+        /// Serializes the data as XML in the object to the designated file path
+        /// </summary>
+        /// <typeparam name="T">Type of Object to serialize</typeparam>
+        /// <param name="dataToSerialize">Object to serialize</param>
+        /// <param name="filePath">FilePath for the XML file</param>
+        public static void SerializeToFile<T>(T dataToSerialize, string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                var writer = new XmlTextWriter(stream, Encoding.Default) { Formatting = Formatting.Indented };
+                new XmlSerializer(typeof(T)).Serialize(writer, dataToSerialize);
+                writer.Close();
+            }
+        }
+
+        public static void SerializeToStream<T>(T dataToSerialize, Stream st)
+        {
+            var writer = new XmlTextWriter(st, Encoding.Default) { Formatting = Formatting.Indented };
+            new XmlSerializer(typeof(T)).Serialize(writer, dataToSerialize);
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Deserializes the data in the XML file into an object
+        /// </summary>
+        /// <typeparam name="T">Type of object to deserialize</typeparam>
+        /// <param name="filePath">FilePath to XML file</param>
+        /// <returns>Object containing deserialized data</returns>
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            try
+            {
+                using (Stream stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                    return (T)(new XmlSerializer(typeof(T))).Deserialize(stream);
+            }
+            catch
+            {
+                return Activator.CreateInstance<T>();
+            }
+        }
+
+        public static T DeserializeFromStream<T>(Stream st)
+        {
+            try
+            {
+                return (T)(new XmlSerializer(typeof(T))).Deserialize(st);
+            }
+            catch
+            {
+                return Activator.CreateInstance<T>();
+            }
+        }
+    }
+}
+
+// ReSharper disable once CheckNamespace
+namespace SerializationServices.JSON
+{
+}
