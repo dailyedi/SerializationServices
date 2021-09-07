728c36872026630f5dd106203641ec8fc5762ae7
Andrew Rober <54873972+AndrewRober@users.noreply.github.com>
2021-09-07 02:58:21 +0200

    up

---
+++ JsonXmlConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationServices
+{
+    public static class JsonXmlConverter
+    {
+        public static string ConvertXmlToJson<T>(string xml) => 
+            JSONSerializationServices.Serialize(XMLSerializationServices.Deserialize<T>(xml));
+
+        public static string ConvertJsonToXml<T>(string json) => 
+            XMLSerializationServices.Serialize(JSONSerializationServices.Deserialize<T>(json));
+    }
+}
